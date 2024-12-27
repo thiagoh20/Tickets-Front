961795206663190bf6977827799390dfaf782369
Alejou343 <alejandro.auribe1@gmail.com>
2024-12-27 18:37:04 -0500

    Fix: Environment variables

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,11 +5,7 @@ export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-black p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
-          </div>
-        </div>
+        <AcmeLogo />
         <LoginForm />
       </div>
     </main>
