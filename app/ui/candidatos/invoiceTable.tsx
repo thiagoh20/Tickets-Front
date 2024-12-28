998ec48aa82cae7eeb3f09f9142ee8d459a02c7a
Alejou343 <alejandro.auribe1@gmail.com>
2024-12-28 15:59:14 -0500

    Feat: Invoices Table

--- app/ui/candidatos/invoiceTable.tsx
+++ app/ui/candidatos/invoiceTable.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 import TicketStatus from '@/app/ui/candidatos/status';
 import { formatDateToLocal } from '@/app/lib/utils';
-import {  fetchFilteredCandidatos } from '@/app/lib/data';
+import { fetchFilteredInvoices } from '@/app/lib/data';
 import { Ticket } from '@/app/lib/definitions';
+import { ArrowDownTrayIcon } from '@heroicons/react/24/outline';
 
 export default async function TableInvoices({
   query,
@@ -13,50 +14,12 @@ export default async function TableInvoices({
   currentPage: number;
   grupo: string;
 }) {
-  const tickets = await fetchFilteredCandidatos(query, currentPage, grupo);
+  const tickets = await fetchFilteredInvoices(query, currentPage, grupo);
 
   return (
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-          <div className="md:hidden">
-            {tickets?.map((ticket: Ticket) => (
-              <div
-                key={ticket.idticket}
-                className="mb-2 w-full rounded-md bg-white p-4"
-              >
-                <div className="flex items-center justify-between border-b pb-4">
-                  <div>
-                    <div className="mb-2 flex items-center">
-                      <Image
-                        src={'/customers/emil-kowalski.png'}
-                        className="mr-2 rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`profile picture`}
-                      />
-                      <p>{ticket.name}</p>
-                    </div>
-                    <p className="text-sm text-gray-500">
-                      {' '}
-                      {ticket.email_person}
-                    </p>
-                  </div>
-                  {/* <CandidatoStatus status={candidatos.estado_proceso} /> */}
-                  {/* <InvoiceStatus status={invoice.status} /> */}
-                </div>
-                <div className="flex w-full items-center justify-between pt-4">
-                  {/* <div>
-                    <p>{"Ingreso: " + formatDateToLocal(candidatos.fecha_ingreso)}</p>
-                    <p>{"Envio: " + formatDateToLocal(candidatos.fecha_envio)}</p>
-                  </div> */}
-                  <div className="flex justify-end gap-2">
-                    {/* <UpdateInvoice id={candidatos.id} grupo={grupo} page={currentPage} /> */}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
@@ -84,44 +47,32 @@ export default async function TableInvoices({
               </tr>
             </thead>
             <tbody className="bg-white">
-              {tickets?.map((ticket: Ticket) => (
+              {tickets?.map((ticket: any, id: number) => (
                 <tr
-                  key={ticket.idticket}
+                  key={ticket.Mes}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                 >
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
-                      <Image
-                        src={'/customers/emil-kowalski.png'}
-                        className="rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`profile picture`}
-                      />
-                      <p>{ticket.name + ' ' + ticket?.lastname}</p>
+                      <p>PNDEC13_00{id + 1}</p>
                     </div>
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {ticket.identity_type + ': ' + ticket.identity_number}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {ticket?.count_adult + ticket?.count_kid}
-                    {/* {formatCurrency(candidatos.amount)} */}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    <TicketStatus status={ticket.status} />
-
-                    {/* {formatDateToLocal(candidatos.fecha_ingreso)} */}
-                    {/* {formatDateToLocal(candidatos.date)} */}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {formatDateToLocal(ticket.date_ticket)}
-                    {/* <p>{formatDateToLocal(candidatos.fecha_envio)}</p> */}
                   </td>
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                    <div className="flex justify-end gap-3">
-                      {/* <UpdateInvoice id={candidatos.id} grupo={grupo} page={currentPage} /> */}
+                    <div className="flex items-center gap-3">
+                      <p>{ticket.Mes}</p>
                     </div>
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    Parque Norte
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    13 de {ticket.Mes}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {ticket.Total}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3 cursor-pointer">
+                    <ArrowDownTrayIcon width={20} />
                   </td>
                 </tr>
               ))}
