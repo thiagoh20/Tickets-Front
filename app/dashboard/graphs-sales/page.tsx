7f479274009096ad7029db50b857a2cb0602949f
Alejou343 <alejandro.auribe1@gmail.com>
2024-12-30 20:50:22 -0500

    Feat: Sales and interactions plots

---
+++ app/dashboard/graphs-sales/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Graphs from '@/app/ui/graphs';
+import React, { useState } from 'react';
+
+const Page = () => {
+  const [park, setPark] = useState<string | null>(null);
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-2xl font-bold my-[2.5%]">Métricas del {park == 'PN' ? 'Parque Norte' : 'Aeroparque'}</h1>
+      <div className="flex items-center p-2 justify-between w-full max-w-sm mx-auto space-x-2">
+        <button
+          onClick={() => setPark('PN')}
+          className={`flex-1 h-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded transition duration-300 ${
+            park === 'PN' ? 'ring-2 ring-blue-300' : ''
+          }`}
+        >
+          Parque Norte
+        </button>
+        <button
+          onClick={() => setPark('AP')}
+          className={`flex-1 h-10 bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded transition duration-300 ${
+            park === 'AP' ? 'ring-2 ring-green-300' : ''
+          }`}
+        >
+          Aeroparque
+        </button>
+      </div>
+      <div className="flex-grow w-full max-w-6xl mx-auto mt-8">
+        {park === 'PN' && <Graphs metric="sales" park="Parque Norte" />}
+        {park === 'AP' && <Graphs metric="sales" park="Aeroparque" />}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- app/ui/graphs/index.tsx
+++ app/ui/graphs/index.tsx
@@ -7,24 +7,17 @@ import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 
-const Graphs = ({ park }: { park: string }) => {
+const Graphs = ({ metric, park }: { metric: string, park: string }) => {
 
     const [data, setData] = useState({
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         datasets: [
             {
-                label: 'Ventas por mes',
+                label: metric == 'sales' ? 'Ventas por mes' : 'Interacciones por mes',
                 data: [6500, 5900, 8000, 8100, 5600, 18270, 5900, 8000, 8100, 5600, 18270, 900],
                 fill: false,
                 backgroundColor: 'rgba(75,192,192,0.4)',
                 borderColor: 'rgba(75,192,192,1)',
-            },
-            {
-                label: 'Devoluciones por mes',
-                data: [182500, 1900, 3000, 4100, 560, 9270, 5300, 800, 810, 560, 170, 900],
-                fill: false,
-                backgroundColor: 'rgba(75,12,192,0.4)',
-                borderColor: 'rgba(75,12,192,1)',
             },
         ],
     });
@@ -32,7 +25,15 @@ const Graphs = ({ park }: { park: string }) => {
     useEffect(() => {
         const fetchInvoices = async () => {
             try {
-                const response = await axios.post(`${process.env.NEXT_PUBLIC_BACK_LINK}/api/marketing/getInvoicesPark`, { idpark: park == 'Parque Norte' ? 1 : 2 });
+                let endpoint: string;
+
+                if (metric === 'sales') {
+                    endpoint = 'getSalesByMonthPark';
+                } else {
+                    endpoint = 'getInteractionsPark';
+                }
+
+                const response = await axios.post(`${process.env.NEXT_PUBLIC_BACK_LINK}/api/marketing/${endpoint}`, { idpark: park == 'Parque Norte' ? 1 : 2 })
                 setData({ 
                     ...data, 
                     labels: response.data.map((invoice: any) => invoice.Mes),
@@ -40,8 +41,7 @@ const Graphs = ({ park }: { park: string }) => {
                         { 
                             ...data.datasets[0], 
                             data: response.data.map((invoice: any) => invoice.Total) 
-                        },
-                        data.datasets[1] // Keep the existing data for "Devoluciones por mes"
+                        }
                     ] 
                 });
             } catch (error) {
@@ -50,7 +50,7 @@ const Graphs = ({ park }: { park: string }) => {
         };
 
         fetchInvoices();
-    }, [data, park]);
+    }, [data, park, metric]);
 
     const options = {
         responsive: true,
@@ -60,7 +60,7 @@ const Graphs = ({ park }: { park: string }) => {
             },
             title: {
                 display: true,
-                text: 'Ventas del último año',
+                text: metric == 'sales' ? 'Ventas del último año' : 'Interacciones del último año',
             },
         },
     };
