9f2e95bbc3f3b649cced9b68e8cd473e196c5515
thiagoh20 <thiago.bbolivar@gmail.com>
2025-01-10 10:05:27 -0500

    [Feature]

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,8 +1,8 @@
 'use client';
 import {
-  AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
+  UserIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
@@ -20,7 +20,7 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="email"
             >
-              Email
+              Nombre de usuario
             </label>
             <div className="relative">
               <input
@@ -28,10 +28,10 @@ export default function LoginForm() {
                 id="email"
                 type="text"
                 name="email"
-                placeholder="Ingrese email"
+                placeholder="Ingrese usuario"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
