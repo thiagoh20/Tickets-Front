e9117788b37af46b6d20899ee676d48ace3b3c5d
thiagoh20 <thiago.bbolivar@gmail.com>
2025-01-16 15:37:43 -0500

    [Feature] se corrigue bug de eliminar

--- app/ui/tickets/buttons.tsx
+++ app/ui/tickets/buttons.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { deleteCandidato } from '@/app/lib/actions';
+
 import { useState } from 'react';
 
 export function CreateInvoice({
@@ -37,60 +37,5 @@ export function UpdateInvoice({ id, grupo, page }: { id: string, grupo: string, 
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
-  const [confirmDelete, setConfirmDelete] = useState(false);
-
-  const handleDeleteClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    setConfirmDelete(true); 
-  };
-
-  const handleConfirmDelete = () => {
-    deleteCandidato(id).then(() => {
-      window.location.reload(); 
-    })
-      .catch((error) => {
-        console.error("Error eliminando el candidato: ", error);
-      });
-    setConfirmDelete(false); 
-  };
-
-  const handleCancelDelete = () => {
-    setConfirmDelete(false); 
-  };
-
-  return (
-    <div>
-      {confirmDelete ? (
-        <div className="flex items-center space-x-2">
-          <button
-            className="flex items-center rounded-md bg-red-500 px-4 py-2 text-sm text-white transition-colors hover:bg-red-400"
-            onClick={handleConfirmDelete}
-          >
-            <TrashIcon className="w-5 mr-2" />
-            Confirmar
-          </button>
-
-          <button
-            className="rounded-md border p-2 hover:bg-gray-100"
-            onClick={handleCancelDelete}
-          >
-            Cancelar
-          </button>
-        </div>
-      ) : (
-        <form>
-          <button
-            className="rounded-md border p-2 hover:bg-gray-100"
-            onClick={handleDeleteClick}
-          >
-            <span className="sr-only">Delete</span>
-            <TrashIcon className="w-5" />
-          </button>
-        </form>
-      )}
-    </div>
-  );
-}
 
 
