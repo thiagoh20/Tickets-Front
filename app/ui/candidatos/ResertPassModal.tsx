3c540a7e319cfec96e22c03a2d520c7e04fc10ac
thiagoh20 <thiago.bbolivar@gmail.com>
2025-02-04 09:19:49 -0500

    [Feature] se añade de denuevo 6 dijitos en la contraseña

--- app/ui/candidatos/ResertPassModal.tsx
+++ app/ui/candidatos/ResertPassModal.tsx
@@ -26,8 +26,8 @@ export default function ResetPasswordModal({
       return;
     }
 
-    if (password.length <3) {
-      setError('La contraseña debe tener al menos 4 caracteres');
+    if (password.length <5) {
+      setError('La contraseña debe tener al menos 6 caracteres');
       return;
     }
 
