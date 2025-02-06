2ca9d571436b1e362c5dc82d3cc468e6b2f0b553
Alejou343 <alejandro.auribe1@gmail.com>
2025-02-06 18:48:20 -0500

    Feat: Redentions by date

--- app/dashboard/redenciones/page.tsx
+++ app/dashboard/redenciones/page.tsx
@@ -6,129 +6,68 @@ import DataTable from 'react-data-table-component';
 
 const Page = () => {
     const [data, setData] = useState([])
-    const [item, setItem] = useState<any>(null)
-    const [openModal, setOpenModal] = useState<boolean>(false)
-    const [date, setDate] = useState<any>(new Date().toISOString().split('T')[0])    
+    const [initialDate, setInitialDate] = useState<any>("2025-01-01")    
+    const [finalDate, setFinalDate] = useState<any>(new Date().toISOString().split('T')[0])    
 
     useEffect(() => {
         const fetchRedentionsByDate = async () => {
             try {
-                await axios.post(`/api/taquilla/redentionsByDate`, { date: date })
+                await axios.post(`http://localhost:80/api/taquilla/redentionsByDate`, { initialDate: initialDate, finalDate: finalDate })
                 .then((response) => setData(response.data))
                 .catch((error) => { setData([]) });
             } catch (error) {
                 console.error("Error fetching disabled days: ", error);
             }
         };
-        console.log(date)
         fetchRedentionsByDate();
-    }, [date]);
-
-    const handleModalOpen = (row: any) => {
-        setItem(row)
-        setOpenModal(!openModal)
-    }
+    }, [initialDate, finalDate]);
 
     // Definición de columnas
     const columns: any = [
         { 
-            name: 'ID', 
-            selector: (row: any) => row.user_id, 
-            sortable: true,
-        },
-        { 
-            name: 'Nombre', 
-            selector: (row: any) => row.nombre_usuario, 
-            sortable: true 
-        },
-        { 
             name: 'Parque', 
-            selector: (row: any) => row.parque_id == 1 ? 'Parque Norte' : 'Aeroparque', 
+            selector: (row: any) => row.idpark == 1 ? 'Parque Norte' : 'Aeroparque Juan Pablo II', 
             sortable: true 
         },
         { 
-            name: '# Redenciones', 
-            selector: (row: any) => row.total_redimidos, 
+            name: 'Descripción', 
+            selector: (row: any) => row.Referencia_Pasaporte, 
             sortable: true 
         },
         { 
-            name: 'Detalle', 
-            selector: (row: any) => <EyeIcon className="h-[18px] w-[18px] cursor-pointer" onClick={() => handleModalOpen(row)} />, 
-            sortable: true 
+            name: 'Cantidad Ingresos', 
+            selector: (row: any) => row.Cantidad_Usada, 
+            sortable: true, 
+            width: '25%'
         }
     ];
 
 return (
     <>
-    {openModal &&     
-    <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative w-[40%] rounded-lg bg-white p-6 text-center shadow-md">
-        <button
-          className="absolute right-2 top-2 text-gray-500 hover:text-gray-700"
-          onClick={() => setOpenModal(!openModal)}
-        >
-          <XMarkIcon width={25} />
-        </button>
-          <div className="rounded-lg border mx-auto bg-white p-6 shadow-md">
-            <h2 className="mb-4 text-lg font-bold">Redenciones del día {item.fecha_redencion.slice(0,10)} {item.parque_id == 1 ? 'Parque Norte' : 'Aeroparque Juan Pablo II'}</h2>
-            <div className="mb-6">
-            </div>
-            <hr className="mb-6" />
-            <div className='overflow-auto'>
-            <table className="w-full border-collapse border border-gray-300">
-              <thead className="bg-gray-100 sticky top-0">
-                <tr>
-                  <th className="md:text-lg text-xs border border-gray-300 px-4 py-2">Tipo de pasaporte</th>
-                  <th className="md:text-lg text-xs border border-gray-300 px-4 py-2">Cantidad</th>
-                </tr>
-              </thead>
-              <tbody className='h-[50%]'>
-                    {['Pasaporte acúatico adulto', 'Pasaporte acúatico niño', 'Ingreso general'].map(x => 
-                    <tr key={x}>
-                        <td className="md:text-md text-xs border border-gray-300 px-4 py-2">
-                            {x}
-                        </td>
-                        <td className="md:text-md text-xs border border-gray-300 px-4 py-2">
-                        8
-                        </td>
-                    </tr>
-                    )}
-              </tbody>
-            </table>
-            </div>
-            <div className="mt-4">
-              <p>
-                <strong>Total Amount:</strong> &nbsp; COP
-              </p>
-            </div>
-          </div>
-        <div className="mt-[2.5%]  flex justify-center gap-[2rem]">
-          <button
-            className="mb-2 rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
-            onClick={() => setOpenModal(!openModal)}
-          >
-            Cerrar
-          </button>
-        </div>
-      </div>
-    </div>}
         <div className='w-[100%] md:w-[50%] mx-auto flex flex-col justify-center items-center h-screen'>
             <h1  className='text-2xl font-bold my-[2.5%] text-center md:absolute md:top-[5rem]'> Verificar las redenciones por fecha </h1>
             <div className="flex justify-center items-center gap-[2rem] mb-[2rem]">
-                <label id="Date" className='font-semibold'>Selecciona la fecha</label>
+                <label id="initialDate" className='font-semibold'>Selecciona la fecha inicial</label>
                 <input 
                 type='date'
-                value={date} 
+                value={initialDate} 
                 className='border-2 border-gray-300 rounded-lg p-2 text-lg focus:outline-none focus:ring-2 focus:ring-blue-500' 
-                onChange={(e) => setDate(e.target.value)} 
+                onChange={(e) => setInitialDate(e.target.value)} 
+                />
+                <label id="initialDate" className='font-semibold'>Selecciona la fecha final</label>
+                <input 
+                type='date'
+                value={finalDate} 
+                className='border-2 border-gray-300 rounded-lg p-2 text-lg focus:outline-none focus:ring-2 focus:ring-blue-500' 
+                onChange={(e) => setFinalDate(e.target.value)} 
                 />
             </div>
             {data.length ? <DataTable
                     columns={columns}
                     data={data}
                     pagination
-                    paginationPerPage={5} // Número de filas por página
-                    paginationRowsPerPageOptions={[5, 10]} // Opciones de filas por página
+                    paginationPerPage={10} // Número de filas por página
+                    paginationRowsPerPageOptions={[10]} // Opciones de filas por página
                     customStyles = {{
                         header: {
                         style: {
@@ -182,7 +121,7 @@ return (
                         },
                         },
                     }}                        
-            /> : <p className='text-lg text-red-300 mt-4'> No hay redenciones del usuario para la fecha {date} </p>}
+            /> : <p className='text-lg text-red-300 mt-4'> No hay redenciones del usuario para el período {initialDate} - {finalDate} </p>}
         </div>
     </>
 );
