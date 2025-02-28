dba3c817763083b81f2ba1cb77fb256e85cad879
Alejou343 <alejandro.auribe1@gmail.com>
2025-02-28 11:09:57 -0500

    Feat: Inform with park name

--- app/dashboard/generar-excel/route.tsx
+++ app/dashboard/generar-excel/route.tsx
@@ -17,6 +17,7 @@ export async function GET(request: any, { params }: any) {
 
     const headers = [
       'ID',
+      'Parque',
       'Nombre',
       'Apellido',
       'Correo',
@@ -45,29 +46,30 @@ export async function GET(request: any, { params }: any) {
     // Iterar sobre los datos de los tickets y agregarlos al Excel
     tickets.forEach((ticket: any, rowIndex: any) => {
       sheet.cell(rowIndex + 2, 1).value(ticket.idticket);
-      sheet.cell(rowIndex + 2, 2).value(ticket.name);
-      sheet.cell(rowIndex + 2, 3).value(ticket.lastname);
-      sheet.cell(rowIndex + 2, 4).value(ticket.email_person);
-      sheet.cell(rowIndex + 2, 5).value(ticket.phone_number);
-      sheet.cell(rowIndex + 2, 6).value(ticket.identity_type);
-      sheet.cell(rowIndex + 2, 7).value(ticket.identity_number);
-      sheet.cell(rowIndex + 2, 8).value(new Date(ticket.createdAt).toISOString().slice(0, 10));
+      sheet.cell(rowIndex + 2, 2).value(ticket.idpark === 1 ? 'Parque Norte' : 'Aeroparque Juan Pablo II');
+      sheet.cell(rowIndex + 2, 3).value(ticket.name);
+      sheet.cell(rowIndex + 2, 4).value(ticket.lastname);
+      sheet.cell(rowIndex + 2, 5).value(ticket.email_person);
+      sheet.cell(rowIndex + 2, 6).value(ticket.phone_number);
+      sheet.cell(rowIndex + 2, 7).value(ticket.identity_type);
+      sheet.cell(rowIndex + 2, 8).value(ticket.identity_number);
+      sheet.cell(rowIndex + 2, 9).value(new Date(ticket.createdAt).toISOString().slice(0, 10));
       sheet
-      .cell(rowIndex + 2, 9)
+      .cell(rowIndex + 2, 10)
       .value(
         ticket.date_ticket
-        ? new Date(ticket.date_ticket).toISOString().slice(0, 10)
+        ? new Date(ticket.date_ticket).toISOString().slice(0, 11)
         : '',
       );
-      sheet.cell(rowIndex + 2, 10).value(ticket.price_ticket);
-      sheet.cell(rowIndex + 2, 11).value(ticket.type_pay);
-      sheet.cell(rowIndex + 2, 12).value(ticket.type_money);
-      sheet.cell(rowIndex + 2, 13).value(ticket.ticket_code);
-      sheet.cell(rowIndex + 2, 14).value(ticket.id_operation);
-      sheet.cell(rowIndex + 2, 15).value(ticket.user_id);
-      sheet.cell(rowIndex + 2, 16).value(ticket.channel);
-      sheet.cell(rowIndex + 2, 17).value(ticket.details);
-      sheet.cell(rowIndex + 2, 18).value(ticket.status);
+      sheet.cell(rowIndex + 2, 12).value(ticket.price_ticket);
+      sheet.cell(rowIndex + 2, 13).value(ticket.type_pay);
+      sheet.cell(rowIndex + 2, 14).value(ticket.type_money);
+      sheet.cell(rowIndex + 2, 15).value(ticket.ticket_code);
+      sheet.cell(rowIndex + 2, 16).value(ticket.id_operation);
+      sheet.cell(rowIndex + 2, 17).value(ticket.user_id);
+      sheet.cell(rowIndex + 2, 18).value(ticket.channel);
+      sheet.cell(rowIndex + 2, 19).value(ticket.details);
+      sheet.cell(rowIndex + 2, 20).value(ticket.status);
     });
     const arrayBuffer = await workbook.outputAsync();
     return new Response(arrayBuffer, {
